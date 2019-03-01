7c29de07b48c87fda491213965cb8c741c03d617
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-01 11:03:35 -0800

    Add endpoints to Book Controller.

--- src/main/java/com/lambdaschool/javabookstore/repositories/AuthorRepository.java
+++ src/main/java/com/lambdaschool/javabookstore/repositories/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.lambdaschool.javabookstore.repositories;
 import com.lambdaschool.javabookstore.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<Author> findByLnameContainingOrFnameContaining(String lname, String fname);
 }
--- src/main/java/com/lambdaschool/javabookstore/repositories/BookRepository.java
+++ src/main/java/com/lambdaschool/javabookstore/repositories/BookRepository.java
@@ -3,5 +3,8 @@ package com.lambdaschool.javabookstore.repositories;
 import com.lambdaschool.javabookstore.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainsIgnoreCase(String name);
 }
