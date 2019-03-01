80c755815c88a1e01dacc3a239d5efc0de62e5a1
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-01 08:24:48 -0800

    Add book model.

---
+++ src/main/java/com/lambdaschool/javabookstore/models/Book.java
@@ -0,0 +1,59 @@
+package com.lambdaschool.javabookstore.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookid;
+    private String booktitle;
+    private String isbn;
+    private int copy;
+
+    @ManyToMany
+    @JoinTable(name = "wrote",
+            joinColumns = {@JoinColumn(name = "bookid")},
+            inverseJoinColumns = {@JoinColumn(name = "authorid")})
+    @JsonIgnoreProperties("authors")
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
+}
