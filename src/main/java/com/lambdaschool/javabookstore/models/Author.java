d0c0cd4a9f7dbf41c8fcea4cb1974063c29de951
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-01 08:31:14 -0800

    Add author model.  Update readme to match change.

---
+++ src/main/java/com/lambdaschool/javabookstore/models/Author.java
@@ -0,0 +1,52 @@
+package com.lambdaschool.javabookstore.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue
+    private long authorid;
+
+    private String lastname;
+    private String firstname;
+
+    @ManyToMany(mappedBy = "authors")
+    @JsonIgnoreProperties("authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public long getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(long authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+}
